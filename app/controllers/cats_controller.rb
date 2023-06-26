b202572d9664890e3bd1c0b58836775dff4ebbcf
bShinzato <bshinzato@outlook.com>
2023-06-26 16:06:00 -0700

    completed, api-endpoints for index, create, update, destroy.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,29 @@
 class CatsController < ApplicationController
+    def index
+        cats = Cat.all
+        render json: cats
+    end
+
+    def create
+        cat = Cat.create(thunder_cat_params)
+        render json: cat
+    end
+
+    def update
+        cat = Cat.find(params[:id])
+        cat.update(thunder_cat_params)
+        render json: cat
+    end
+
+    def destroy
+        cat = Cat.find(params[:id])
+        cat.destroy
+        render json: cat
+    end
+
+
+    private
+    def thunder_cat_params
+        params.require(:cat).permit(:name, :age, :enjoys, :image)
+    end
 end
